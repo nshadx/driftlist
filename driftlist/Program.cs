9937db9788c7b4a0ef4412ca793121adc6c14547
korabelnikov <korabelnikov@rza.ru>
2026-06-25 02:27:56 +0300

    vec

--- driftlist/Program.cs
+++ driftlist/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System.Diagnostics;
 
-double[][] prob =
+double[] vecA = [1.0, 1.0]; // phonk, aggressive
+double[] vecB = [1.0, 0.6]; // phonk, dark
+double[] vecC = [0.2, 0.1]; // rap, calm
+double[] vecD = [0.2, 0.2]; // rap, sad
+double[] vecE = [0.2, 1.0]; // rap, aggressive
+
+double[][] vec =
 [
-    [ 0.0, 0.6, 0.0, 0.1, 0.3 ],
-    [ 0.5, 0.0, 0.0, 0.4, 0.1 ],
-    [ 0.0, 0.1, 0.0, 0.6, 0.3 ],
-    [ 0.0, 0.1, 0.6, 0.0, 0.3 ],
-    [ 0.4, 0.1, 0.0, 0.5, 0.0 ],
+    vecA,
+    vecB,
+    vecC,
+    vecD,
+    vecE,
 ];
+
+var prob = GetProb(vec);
 
 Track a = new("phonk", "aggressive");
 Track b = new("phonk", "dark");
@@ -56,6 +64,65 @@ Track Transition(int id)
     throw new UnreachableException();
 }
 
-double SoftMax()
+double[][] GetProb(double[][] vec)
+{
+    var prob = new double[vec.Length][];
+    
+    for (var i = 0; i < prob.Length; i++)
+    {
+        prob[i] = new double[vec.Length];
+        
+        for (var j = 0; j < prob[i].Length; j++)
+        {
+            prob[i][j] = CosineSimilarity(vec[i], vec[j]);
+        }
+    }
+    
+    for (var i = 0; i < prob.Length; i++)
+    {
+        prob[i] = SoftMax(prob[i]);
+    }
+    
+    for (var i = 0; i < prob.Length; i++)
+    {
+        prob[i][i] = 0;
+        var s = prob[i].Sum();
+        for (var j = 0; j < prob[i].Length; j++)
+        {
+            prob[i][j] /= s;
+        }
+    }
+    
+    return prob;
+}
+
+double[] SoftMax(double[] arr)
+{
+    var s = arr.Sum(Math.Exp);
+    return arr.Select(x => Math.Exp(x) / s).ToArray();
+}
+
+double CosineSimilarity(double[] a, double[] b)
+{
+    // |a| = |b|
+    var scalar = 0d;
+    for (var i = 0; i < a.Length; i++)
+    {
+        scalar += a[i] * b[i];
+    }
+    
+    return scalar / (GetLength(a) * GetLength(b));
+}
+
+double GetLength(double[] vec)
+{
+    var sum = 0d;
+    for (var i = 0; i < vec.Length; i++)
+    {
+        sum += Math.Pow(vec[i], 2);
+    }
+    
+    return Math.Sqrt(sum);
+}
 
 internal record Track(string Genre, string Mood);
